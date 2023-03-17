c28c4339156994bbc4dd3b881e2ea696bf6d9259
Robin.Wang <3Jg$94sbQ@q1>
2023-03-17 14:37:10 +0800

    feat(初始化): 添加sass、element-plus

---
+++ src/components/layout/AppContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="common-layout h-full">
+    <el-container class="h-full">
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
