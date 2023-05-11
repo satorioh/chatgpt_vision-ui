7d63d66600477dec28894b51d8aa2ab3cc0fbe35
Robin.Wang <3Jg$94sbQ@q1>
2023-05-11 17:00:35 +0800

    feat(首页): 添加回复框

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -2,24 +2,51 @@
   <div class="home-container">
     <div class="title">测试</div>
     <div class="textarea-wrap">
-      <el-input v-model="inputValue" :rows="3" type="textarea" placeholder="请输入您想咨询的问题" />
+      <el-input v-model="inputValue" :rows="5" type="textarea" placeholder="请输入您想咨询的问题" />
     </div>
     <div class="btn-wrap">
-      <el-button type="primary" @click="onClick">发送</el-button>
+      <el-button type="primary" :disabled="!inputValue" :loading="loading" @click="onClick"
+        >发送</el-button
+      >
+    </div>
+    <div class="answer-wrap">
+      <div class="font-normal">{{ loadingHint }}</div>
+      <el-input v-model="answerText" :rows="5" type="textarea" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { getAnswer } from "@/api";
 
 const inputValue = ref("");
+const answerText = ref("");
+const loading = ref(false);
+
+const showLoading = (flag: boolean) => {
+  loading.value = flag;
+};
+
+const loadingHint = computed(() => {
+  return loading.value ? "AI 思考中..." : "AI 的回复";
+});
+
+const changeAnswerText = (text: string) => {
+  answerText.value = text;
+};
 
 const onClick = async () => {
-  console.log("onClick");
-  const res = await getAnswer({ query: inputValue.value });
-  console.log(res);
+  try {
+    showLoading(true);
+    const res = await getAnswer({ query: inputValue.value });
+    console.log(res);
+    changeAnswerText(res.data.answer);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    showLoading(false);
+  }
 };
 </script>
 
